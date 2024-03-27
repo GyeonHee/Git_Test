6309a550ae5ac1375088435f9a63c9c003c6b869
GyeonHee <104518499+GyeonHee@users.noreply.github.com>
2024-03-27 11:01:52 +0900

    int c = 3;

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@ int main()
 {
 	int a = 1;
 	int b = 2;
+	int c = 3;
 	std::cout << "hi" << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+	std::cout << c << std::endl;
 	return 0;
 }
